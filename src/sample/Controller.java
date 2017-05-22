a39e69fbeef35d43670a5d2cbfe59f1b60899ef9
Jonas Schmidbauer <jonas.schmidbauer@technikum-wien.at>
2017-05-22 21:39:20 +0200

    Added UI and Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    private BlindsProxy blinds;
+
+    public Controller() {
+        blinds = new BlindsProxy();
+    }
+
+    @FXML
+    private void buttonUpAction(ActionEvent event) {
+        blinds.up();
+    }
+
+    @FXML
+    private void buttonStopAction(ActionEvent event) {
+        blinds.stop();
+    }
+
+    @FXML
+    private void buttonDownAction(ActionEvent event) {
+        blinds.down();
+    }
 }
