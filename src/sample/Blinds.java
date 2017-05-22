ecd4502ce46ee497235c37240102399f6ca4ec46
Jonas Schmidbauer <jonas.schmidbauer@technikum-wien.at>
2017-05-22 16:54:08 +0200

    Grundstruktur Proxy erstellt

---
+++ src/sample/Blinds.java
@@ -0,0 +1,9 @@
+package sample;
+
+/**
+ * Created by jonasschmides on 22.05.17.
+ */
+public interface Blinds {
+    public void up();
+    public void down();
+}
---
+++ src/sample/BlindsImpl.java
@@ -0,0 +1,16 @@
+package sample;
+
+/**
+ * Created by jonasschmides on 22.05.17.
+ */
+public class BlindsImpl implements Blinds {
+    @Override
+    public void up() {
+
+    }
+
+    @Override
+    public void down() {
+
+    }
+}
